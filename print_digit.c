716ca9fec6030fd51a117cf692ef0264d6db4ccb
Adeola2055 <iadeola2055@gmail.com>
2024-02-05 09:10:43 +0100

    legendary mode

--- print_digit.c
+++ print_digit.c
@@ -1,18 +1,38 @@
 #include "main.h"
+#include <limits.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 /**
+ * int_min_checker - check if num is INT_MIN
+ * @num: points to num
+ * @count: points to count
+ *
+ * Return: number of count
+ */
+void int_min_checker (int *num, int *count)
+{
+
+	if (*num == INT_MIN)
+	{
+		_putchar('-');
+		*num = *num + 1;
+		*num = -(*num);
+		(*count)++;
+	}
+}
+/**
  * print_digit - print a digit
  * @num: digit to print
  *
- * Return: number of count
+ * Return: number of digit printed to stdout
  */
 int print_digit(int num)
 {
 	char *ptr;
 	int count = 0, temp = num, i = 0, j, len = 0;
 
+	int_min_checker(&num, &count);
 	if (num < 0)
 	{
 		_putchar('-');
